539e752a36a37fcf50ba5e19399034c2897267c9
VijayHYadav <vizzyadav1997@gmail.com>
2022-02-05 17:52:50 +0530

    003 DIsplaying Events

--- dj-events/dj-events-frontent/pages/index.js
+++ dj-events/dj-events-frontent/pages/index.js
@@ -1,11 +1,17 @@
+import EventItem from "@/components/EventItem"
+import Link from 'next/link'
 import Layout from "@/components/Layout"
 import { API_URL } from "@/config/index"
 
 export default function Home({ events }) {
-  console.log(events)
   return (
     <Layout>
       <h1>Upcomming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+      {events.length > 0 && <Link href='/events'><a className="btn-secondary">View All Events</a></Link>}
     </Layout>
   )
 }
@@ -13,9 +19,8 @@ export default function Home({ events }) {
 export async function getStaticProps() {
   const res = await fetch(`${API_URL}/api/events`)
   const events = await res.json()
-  // console.log("hello",events);
   return {
-    props: { events },
+    props: { events: events.slice(3) },
     revalidate: 1
   }
 }
