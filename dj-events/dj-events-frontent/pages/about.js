3abae230be69704aee48d3b184a9764752701e43
VijayHYadav <vizzyadav1997@gmail.com>
2022-02-05 08:33:06 +0530

    003 Next Link & Router

--- dj-events/dj-events-frontent/pages/about.js
+++ dj-events/dj-events-frontent/pages/about.js
@@ -1,9 +1,11 @@
 import React from 'react';
+// import Link from 'next/link';
 
 export default function about() {
   return <div>
-      <h1>About</h1>
-      <p>This is an app to find the lates DJ and other musical events</p>
-      <p>Version :1.0.0</p>
+    <h1>About</h1>
+    <p>This is an app to find the lates DJ and other musical events</p>
+    <p>Version :1.0.0</p>
+    {/* <Link href='/'>Home</Link> */}
   </div>;
 }
--- dj-events/dj-events-frontent/pages/events/[slug].js
+++ dj-events/dj-events-frontent/pages/events/[slug].js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useRouter } from 'next/router'
 
 export default function EventPage() {
+  const router = useRouter()
+
   return <div>
-      <h1>My Event</h1>
+    <h1>My Event</h1>
+    <h3>{router.query.slug}</h3>
+    <button onClick={() => router.push('/')}>Click</button>
   </div>;
 }
