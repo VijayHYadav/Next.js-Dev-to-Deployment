85a95eee9c206e84cd984d077ca1f601ae14d8f0
VijayHYadav <vizzyadav1997@gmail.com>
2022-02-05 12:53:40 +0530

    007 Module Aliases With jsconfig

--- dj-events/dj-events-frontent/pages/events/add.js
+++ dj-events/dj-events-frontent/pages/events/add.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../../components/Layout';
+import Layout from '@/components/Layout';
 
 function AddEventPage() {
   return <Layout title='Add New Event'><h1>AddEventPage</h1></Layout>;
