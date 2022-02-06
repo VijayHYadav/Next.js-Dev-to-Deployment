8a7027dc8346320327406c30c520e417feba11a3
VijayHYadav <vizzyadav1997@gmail.com>
2022-02-07 05:18:44 +0530

    001 Delete Events

--- dj-events/dj-events-frontent/pages/events/[slug].js
+++ dj-events/dj-events-frontent/pages/events/[slug].js
@@ -9,8 +9,22 @@ import styles from '@/styles/Event.module.css'
 import { useRouter } from 'next/router'
 
 export default function EventPage({ evt }) {
-  const deleteEvent = (e) => {
-    console.log('delete')
+  const router = useRouter()
+
+  const deleteEvent = async (e) => {
+    if (confirm('Are you sure?')) {
+      const res = await fetch(`${API_URL}/events/${evt.id}`, {
+        method: 'DELETE'
+      })
+
+      const data = await res.json()
+
+      if (!res.ok) {
+        toast.error(data.message)
+      } else {
+        router.push('/events')
+      }
+    }
   }
 
   return (
